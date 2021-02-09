6fcec54a2f29429a58670c60f975e9f1d42d649c
Mithi Akter <73846125+MiThi-5@users.noreply.github.com>
2021-02-09 23:34:14 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Student_Management_System
+ This project is made by using Java Swing. This is basically a desktop GUI for the student management system.
---
+++ src/student_management_system/StudentTable.java
@@ -0,0 +1,232 @@
+
+package student_management_system;
+
+
+
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public final class StudentTable extends JFrame implements ActionListener {
+    private JTable table;
+    private JScrollPane scroll;
+    private DefaultTableModel model;
+    private Container c;
+    private JLabel titleLabel,fnLabel,lnLabel,phoneLabel,gpaLabel;
+    private JTextField fnTf,lnTf,phoneTf,gpaTf;
+    private JButton addButton,updateButton,deleteButton,clearButton;
+    
+    private String [] columns ={"First Name","Last Name","Phone","GPA"};
+    private String [] rows = new String [4];
+    
+   StudentTable(){
+       initComponents();
+   }
+   public void initComponents(){
+               
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(780,690);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Student table");
+        
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.PINK);
+        
+        Font font = new Font("Arial",Font.BOLD,16);
+        
+        
+        titleLabel= new JLabel ("Sudent registration Form");
+        titleLabel.setFont(font);
+        titleLabel.setBounds(140,10,250,50);
+        c.add(titleLabel);
+        
+        fnLabel =new JLabel ("First Name       ");
+        fnLabel.setBounds (10,80,140,30);
+        fnLabel.setFont(font);
+        c.add(fnLabel);
+        
+        fnTf= new JTextField();
+        fnTf.setBounds(110,80,200,30);
+        fnTf.setFont(font);
+        c.add(fnTf);
+        
+        addButton = new JButton ("Add");
+        addButton.setBounds(400,80,100,30);
+        addButton.setFont(font);
+        c.add(addButton);
+        
+        lnLabel= new JLabel ("Last Name     ");
+        lnLabel.setBounds (10,130,150,30);
+        lnLabel.setFont(font);
+        c.add(lnLabel);
+        
+        lnTf= new JTextField();
+        lnTf.setBounds (110,130,200,30);
+        lnTf.setFont(font);
+        c.add(lnTf);
+        
+        updateButton = new JButton ("Update");
+        updateButton.setBounds (400,130,100,30);
+        updateButton.setFont(font);
+        c.add(updateButton);
+        
+        phoneLabel= new JLabel ("Phone");
+        phoneLabel.setBounds (10,180,150,30);
+        phoneLabel.setFont(font);
+        c.add(phoneLabel);
+        
+        phoneTf = new JTextField();
+        phoneTf.setBounds (110,180,200,30);
+        phoneTf.setFont(font);
+        c.add(phoneTf);
+        
+        deleteButton = new JButton ("Delete");
+        deleteButton.setBounds (400,180,100,30);
+        deleteButton.setFont(font);
+        c.add(deleteButton);
+        
+        gpaLabel= new JLabel ("G.P.A    ");
+        gpaLabel.setBounds (10,230,150,30);
+        gpaLabel.setFont(font);
+        c.add(gpaLabel);
+        
+        gpaTf = new JTextField();
+        gpaTf.setBounds (110,230,200,30);
+        gpaTf.setFont(font);
+        c.add(gpaTf);
+        
+        clearButton = new JButton ("Clear");
+        clearButton.setBounds (400,230,100,30);
+        clearButton.setFont(font);
+        c.add(clearButton);
+        
+        table = new JTable();
+        
+        model = new DefaultTableModel();
+        model.setColumnIdentifiers(columns);
+        table.setModel(model);
+        table.setFont(font);
+        table.setSelectionBackground(Color.GREEN);
+        table.setBackground(Color.WHITE);
+        table.setRowHeight(30);
+        
+        scroll = new JScrollPane(table);
+        scroll.setBounds(10, 360, 740, 265);
+        c.add(scroll);
+        
+        addButton.addActionListener(this);
+        clearButton.addActionListener(this);
+        deleteButton.addActionListener(this);
+        updateButton.addActionListener(this);
+        
+        table.addMouseListener(new MouseAdapter(){
+        
+        
+        @Override
+        public void mouseClicked(MouseEvent me){
+            int numberOfRow = table.getSelectedRow();
+            
+            
+            String f_name=model.getValueAt(numberOfRow, 0).toString();
+            String l_name=model.getValueAt(numberOfRow, 1).toString();
+            String phone=model.getValueAt(numberOfRow, 2).toString();
+            String gpa=model.getValueAt(numberOfRow, 3).toString();
+            
+            fnTf.setText(f_name);
+            lnTf.setText(l_name);
+            phoneTf.setText(phone);
+            gpaTf.setText(gpa);
+      
+        }
+                }
+        
+        );
+      
+        }
+   
+   @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if (e.getSource()== addButton){
+            
+            rows[0]= fnTf.getText();
+            rows[1]= lnTf.getText();
+            rows[2]= phoneTf.getText();
+            rows[3]= gpaTf.getText();
+            model.addRow(rows);
+            
+        }
+       
+        else if (e.getSource()==clearButton){
+        
+        
+        fnTf.setText("");
+        lnTf.setText("");
+        phoneTf.setText("");
+        gpaTf.setText("");
+        }
+        
+        else if (e.getSource()==deleteButton){
+        int numberofRow =table.getSelectedRow();
+        if (numberofRow>=0){
+        model.removeRow(numberofRow);
+        }
+        
+        else{
+            JOptionPane.showMessageDialog(null,"No row has been selected / No row exist");
+            }
+        }
+        
+        else if (e.getSource() == updateButton ){
+        int numberOfRow = table.getSelectedRow();
+                
+        
+        String f_name = fnTf.getText();
+        String l_name = lnTf.getText();
+        String phone = phoneTf.getText();
+        String gpa = gpaTf.getText();
+        
+        
+        
+        model.setValueAt(f_name,numberOfRow , 0);
+        model.setValueAt(l_name,numberOfRow, 1);
+        model.setValueAt(phone,numberOfRow , 2);
+        model.setValueAt(gpa,numberOfRow , 3);
+        
+    }   
+    }
+        
+        public static void main(String[] args){
+        StudentTable frame = new StudentTable();
+        frame.setVisible(true);
+    
+}
+
+}
+   
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
